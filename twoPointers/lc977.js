aad0d47b51a2c850061eb0c5a5c5076c92f6fbc5
Jan Markus Milan <jmarkusm@me.com>
2022-11-08 23:01:35 -0500

    feat: add function for problem statement

--- twoPointers/lc977.js
+++ twoPointers/lc977.js
@@ -1,2 +1,6 @@
 // Problem Statement #
 // Given a sorted array, create a new array containing squares of all the numbers of the input array in the sorted order.
+
+function make_squares(arr) {
+  // TODO: Write code here
+}
