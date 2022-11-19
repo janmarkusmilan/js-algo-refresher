5fbed49958f000306aa4103493a1679ff5837973
Jan Markus Milan <jmarkusm@me.com>
2022-11-18 23:22:51 -0500

    feat: add problem statement for lc340

---
+++ slidingWindow/lc340.js
@@ -0,0 +1,5 @@
+// Problem Statement #
+
+// Given a string, find the length of the longest substring in it with no more than K distinct characters.
+
+// You can assume that K is less than or equal to the length of the given string.
