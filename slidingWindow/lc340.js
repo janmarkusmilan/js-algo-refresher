337b59be3274752d551c7812a3f1b0010259bbae
Jan Markus Milan <jmarkusm@me.com>
2022-11-19 22:57:28 -0500

    feat: add psuedo code for solution

--- slidingWindow/lc340.js
+++ slidingWindow/lc340.js
@@ -4,6 +4,17 @@
 
 // You can assume that K is less than or equal to the length of the given string.
 
+/*
+Hashmap: Keep track of every instance of each character
+Var: max
+Loop (forms a window):
+  add the char to the hashmap (if it is in hashmap, increment)
+  if occurrence of a char > K:
+    slide window
+  update length to max
+return the max
+*/
+
 function longest_substring_with_k_distinct(str, k) {
   // TODO: Write code here
 }
