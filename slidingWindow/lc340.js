682c39704bd851aa11a45d567360963423cc0f8c
Jan Markus Milan <jmarkusm@me.com>
2022-11-20 23:41:32 -0500

    feat: add another sample solution in comments

--- slidingWindow/lc340.js
+++ slidingWindow/lc340.js
@@ -79,3 +79,31 @@ console.log(
     3
   )}`
 );
+
+// Solution
+// -----
+// let windowStart = 0,
+// maxLength = 0,
+// charFrequency = {};
+
+// // in the following loop we'll try to extend the range [window_start, window_end]
+// for (let windowEnd = 0; windowEnd < str.length; windowEnd++) {
+// const rightChar = str[windowEnd];
+// if (!(rightChar in charFrequency)) {
+//   charFrequency[rightChar] = 0;
+// }
+// charFrequency[rightChar] += 1;
+// // shrink the sliding window, until we are left with 'k' distinct characters in the char_frequency
+// while (Object.keys(charFrequency).length > k) {
+//   const leftChar = str[windowStart];
+//   charFrequency[leftChar] -= 1;
+//   if (charFrequency[leftChar] === 0) {
+//     delete charFrequency[leftChar];
+//   }
+//   windowStart += 1; // shrink the window
+// }
+// // remember the maximum length so far
+// maxLength = Math.max(maxLength, windowEnd - windowStart + 1);
+// }
+
+// return maxLength;
