72078ee7e9c29d9462805b3509769523febeb75a
Jan Markus Milan <jmarkusm@me.com>
2022-11-23 21:46:06 -0500

    feat: add solution for problem

--- slidingWindow/lc53.js
+++ slidingWindow/lc53.js
@@ -4,4 +4,19 @@
 
 function max_subarray_size_k(k, arr) {
   // TODO: Write your code here
+  let max = 0;
+  let sum = 0;
+
+  let leftPtr = 0;
+  for (let rightPtr = 0; rightPtr < arr.length; rightPtr++) {
+    sum += arr[rightPtr];
+
+    if (rightPtr >= k - 1) {
+      max = Math.max(max, sum);
+      sum -= arr[leftPtr];
+      leftPtr++;
+    }
+  }
+
+  return max;
 }
