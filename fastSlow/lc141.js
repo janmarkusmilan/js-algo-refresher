d0637914c11e9b0b63de0fe078e4749a921db8d0
Jan Markus Milan <jmarkusm@me.com>
2022-11-26 21:49:19 -0500

    feat: provide class (framework) for problem

--- fastSlow/lc141.js
+++ fastSlow/lc141.js
@@ -1,3 +1,10 @@
 // Problem Statement #
 
 // Given the head of a Singly LinkedList, write a function to determine if the LinkedList has a cycle in it or not.
+
+class Node {
+  constructor(value, next = null) {
+    this.value = value;
+    this.next = next;
+  }
+}
