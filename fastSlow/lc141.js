0e716c908beae91a087f7af4ed05efef28009cc6
Jan Markus Milan <jmarkusm@me.com>
2022-11-27 21:49:28 -0500

    feat: add some test cases

--- fastSlow/lc141.js
+++ fastSlow/lc141.js
@@ -19,3 +19,10 @@ head.next.next = new Node(3);
 head.next.next.next = new Node(4);
 head.next.next.next.next = new Node(5);
 head.next.next.next.next.next = new Node(6);
+console.log(`LinkedList has cycle: ${has_cycle(head)}`);
+
+head.next.next.next.next.next.next = head.next.next;
+console.log(`LinkedList has cycle: ${has_cycle(head)}`);
+
+head.next.next.next.next.next.next = head.next.next.next;
+console.log(`LinkedList has cycle: ${has_cycle(head)}`);
