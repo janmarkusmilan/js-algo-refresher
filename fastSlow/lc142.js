f0fb7bd1b92d173496990a3995a49b4797b7a13e
Jan Markus Milan <jmarkusm@me.com>
2022-11-30 23:11:47 -0500

    feat: provide class to form linked list

--- fastSlow/lc142.js
+++ fastSlow/lc142.js
@@ -1,3 +1,10 @@
 // Problem Statement #
 
 // Given the head of a Singly LinkedList that contains a cycle, write a function to find the starting node of the cycle.
+
+class Node {
+  constructor(value, next = null) {
+    this.value = value;
+    this.next = next;
+  }
+}
