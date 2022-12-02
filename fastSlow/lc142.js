58e2290d606a8c9682fdef912f82c4c31e7e66f2
Jan Markus Milan <jmarkusm@me.com>
2022-12-01 23:54:24 -0500

    feat: add analyis of used algorithm

--- fastSlow/lc142.js
+++ fastSlow/lc142.js
@@ -2,6 +2,19 @@
 
 // Given the head of a Singly LinkedList that contains a cycle, write a function to find the starting node of the cycle.
 
+/*
+Tortoise and Hare Algorithm:
+  P1 where it moves 1 step
+  P2 where it moves 2 steps
+
+  P3 = Hare or Tortoise 
+  P4 = Head node
+
+  Loop until they reach the same node
+  Return one of the pointers at the end
+  Return head
+*/
+
 class Node {
   constructor(value, next = null) {
     this.value = value;
