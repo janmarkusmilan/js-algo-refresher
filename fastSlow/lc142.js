683a3d22e37e7a223cf255c668664e9ed7c9a603
Jan Markus Milan <jmarkusm@me.com>
2022-12-01 23:53:31 -0500

    feat: provide test cases for problem

--- fastSlow/lc142.js
+++ fastSlow/lc142.js
@@ -12,3 +12,18 @@ class Node {
 const find_cycle_start = function (head) {
   // TODO: Write your code here
 };
+
+head = new Node(1);
+head.next = new Node(2);
+head.next.next = new Node(3);
+head.next.next.next = new Node(4);
+head.next.next.next.next = new Node(5);
+head.next.next.next.next.next = new Node(6);
+
+head.next.next.next.next.next.next = head.next.next;
+console.log(`LinkedList cycle start: ${find_cycle_start(head).value}`);
+
+head.next.next.next.next.next.next = head.next.next.next;
+console.log(`LinkedList cycle start: ${find_cycle_start(head).value}`);
+
+head.next.next.next.next.next.next = head;
