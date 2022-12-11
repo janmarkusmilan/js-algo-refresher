980e99cee1810420585b5a2b8b38efe73353aa38
Jan Markus Milan <jmarkusm@me.com>
2022-12-10 21:54:15 -0500

    feat: provide problem statement

---
+++ fastSlow/lc876.js
@@ -0,0 +1,5 @@
+// Problem Statement #
+
+// Given the head of a Singly LinkedList, write a method to return the middle node of the LinkedList.
+
+// If the total number of nodes in the LinkedList is even, return the second middle node.
