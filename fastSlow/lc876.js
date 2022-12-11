d32629ed7699eaff9274b245bdadfef22167fbf0
Jan Markus Milan <jmarkusm@me.com>
2022-12-10 21:54:41 -0500

    feat: provide class for linked list

--- fastSlow/lc876.js
+++ fastSlow/lc876.js
@@ -3,3 +3,10 @@
 // Given the head of a Singly LinkedList, write a method to return the middle node of the LinkedList.
 
 // If the total number of nodes in the LinkedList is even, return the second middle node.
+
+class Node {
+  constructor(value, next = null) {
+    this.value = value;
+    this.next = next;
+  }
+}
