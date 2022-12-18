eae83d06b566773a2cd13d3a73132b477c967019
Jan Markus Milan <jmarkusm@me.com>
2022-12-17 23:35:03 -0500

    feat: provide class for problem statement

--- mergeIntervals/lc252.js
+++ mergeIntervals/lc252.js
@@ -1,3 +1,14 @@
 // Problem Statement #
 
 // Given an array of intervals representing ‘N’ appointments, find out if a person can attend all the appointments.
+
+class Interval {
+  constructor(start, end) {
+    this.start = start;
+    this.end = end;
+  }
+
+  print_interval() {
+    process.stdout.write(`[${this.start}, ${this.end}]`);
+  }
+}
