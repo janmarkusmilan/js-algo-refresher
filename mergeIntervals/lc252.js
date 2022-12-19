de1af0a3ae60a4a177a807f8d8a92ab535383c7d
Jan Markus Milan <jmarkusm@me.com>
2022-12-18 22:43:47 -0500

    feat: add sample test cases

--- mergeIntervals/lc252.js
+++ mergeIntervals/lc252.js
@@ -16,3 +16,27 @@ class Interval {
 const can_attend_all_appointments = function (intervals) {
   // TODO: Write your code here
 };
+
+console.log(
+  `Can attend all appointments: ${can_attend_all_appointments([
+    new Interval(1, 4),
+    new Interval(2, 5),
+    new Interval(7, 9),
+  ])}`
+);
+
+console.log(
+  `Can attend all appointments: ${can_attend_all_appointments([
+    new Interval(6, 7),
+    new Interval(2, 4),
+    new Interval(8, 12),
+  ])}`
+);
+
+console.log(
+  `Can attend all appointments: ${can_attend_all_appointments([
+    new Interval(4, 5),
+    new Interval(2, 3),
+    new Interval(3, 6),
+  ])}`
+);
