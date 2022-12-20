5db63d165249dff4b00f00d8c7920443fc5a8fb3
Jan Markus Milan <jmarkusm@me.com>
2022-12-19 23:51:55 -0500

    feat: add solution to problem

--- mergeIntervals/lc252.js
+++ mergeIntervals/lc252.js
@@ -23,6 +23,18 @@ class Interval {
 
 const can_attend_all_appointments = function (intervals) {
   // TODO: Write your code here
+  intervals.sort((a, b) => a.start - b.start);
+
+  for (let i = 1; i < intervals.length; i++) {
+    if (
+      intervals[i].start > intervals[i - 1].start &&
+      intervals[i].start < intervals[i - 1].end
+    ) {
+      return false;
+    }
+  }
+
+  return true;
 };
 
 console.log(
