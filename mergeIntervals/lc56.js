1e1619d521ee255c0fccac961a5e8292e3ba1aa0
Jan Markus Milan <jmarkusm@me.com>
2022-12-22 23:56:18 -0500

    feat: provide class for problem

--- mergeIntervals/lc56.js
+++ mergeIntervals/lc56.js
@@ -1,3 +1,14 @@
 // Problem Statement #
 
 // Given a list of intervals, merge all the overlapping intervals to produce a list that has only mutually exclusive intervals.
+
+class Interval {
+  constructor(start, end) {
+    this.start = start;
+    this.end = end;
+  }
+
+  get_interval() {
+    return "[" + this.start + ", " + this.end + "]";
+  }
+}
