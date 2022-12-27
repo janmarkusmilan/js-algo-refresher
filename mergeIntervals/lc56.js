9868a85a0e1341b779dab0cd1746b023b5913a05
Jan Markus Milan <jmarkusm@me.com>
2022-12-26 23:53:08 -0500

    feat: provide test cases

--- mergeIntervals/lc56.js
+++ mergeIntervals/lc56.js
@@ -16,3 +16,43 @@ class Interval {
 const merge = function (intervals) {
   // TODO: Write your code here
 };
+
+merged_intervals = merge([
+  new Interval(1, 4),
+  new Interval(2, 5),
+  new Interval(7, 9),
+]);
+result = "";
+for (i = 0; i < merged_intervals.length; i++) {
+  result += merged_intervals[i].get_interval() + " ";
+}
+console.log(`Merged intervals: ${result}`);
+
+merged_intervals = merge([
+  new Interval(6, 7),
+  new Interval(2, 4),
+  new Interval(5, 9),
+]);
+result = "";
+for (i = 0; i < merged_intervals.length; i++) {
+  result += merged_intervals[i].get_interval() + " ";
+}
+console.log(`Merged intervals: ${result}`);
+
+merged_intervals = merge([
+  new Interval(1, 4),
+  new Interval(2, 6),
+  new Interval(3, 5),
+]);
+result = "";
+for (i = 0; i < merged_intervals.length; i++) {
+  result += merged_intervals[i].get_interval() + " ";
+}
+console.log(`Merged intervals: ${result}`);
+
+merged_intervals = merge([new Interval(1, 4), new Interval(1, 4)]);
+result = "";
+for (i = 0; i < merged_intervals.length; i++) {
+  result += merged_intervals[i].get_interval() + " ";
+}
+console.log(`Merged intervals: ${result}`);
