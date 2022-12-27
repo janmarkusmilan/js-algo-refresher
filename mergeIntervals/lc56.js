f2b4c9fe230e6b3b5154e0a85ff0f5409ec16a3a
Jan Markus Milan <jmarkusm@me.com>
2022-12-26 23:54:28 -0500

    feat: space out thoughts for readability

--- mergeIntervals/lc56.js
+++ mergeIntervals/lc56.js
@@ -5,16 +5,20 @@
 /*
 [[6, 7], [2, 4], [5, 9]]
 Sort: [[2, 4], [5, 9], [6, 7]]
+
 Vars:
   res array (put our result)
   ptr1: arr[0][0] = 2
   ptr2: arr[0][1] = 4
+
 Loop through the rest of the intervals
 Have a comparison:
   If it is overlapping: update one of our ptrs and to res (get the lowest and greatest values between the two intervals)
   If it is not overlapping: push the first arr into res and update the two pointers to the next interval in the array
+
 ptr1: arr[1][0] = 5
 ptr2: arr[1][1] = 9
+
 The end, the res array should have our merged intervals
 */
 
