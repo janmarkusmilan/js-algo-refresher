af5ec825ff14568e1a6ddd38f4b28c25fc040527
Jan Markus Milan <jmarkusm@me.com>
2022-12-26 23:53:48 -0500

    feat: write down thoughts on solution

--- mergeIntervals/lc56.js
+++ mergeIntervals/lc56.js
@@ -2,6 +2,22 @@
 
 // Given a list of intervals, merge all the overlapping intervals to produce a list that has only mutually exclusive intervals.
 
+/*
+[[6, 7], [2, 4], [5, 9]]
+Sort: [[2, 4], [5, 9], [6, 7]]
+Vars:
+  res array (put our result)
+  ptr1: arr[0][0] = 2
+  ptr2: arr[0][1] = 4
+Loop through the rest of the intervals
+Have a comparison:
+  If it is overlapping: update one of our ptrs and to res (get the lowest and greatest values between the two intervals)
+  If it is not overlapping: push the first arr into res and update the two pointers to the next interval in the array
+ptr1: arr[1][0] = 5
+ptr2: arr[1][1] = 9
+The end, the res array should have our merged intervals
+*/
+
 class Interval {
   constructor(start, end) {
     this.start = start;
