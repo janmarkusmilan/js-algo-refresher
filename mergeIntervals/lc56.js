5d7fe51f4942cdd1f88a7bdb9b5237915c1982bb
Jan Markus Milan <jmarkusm@me.com>
2022-12-28 23:21:59 -0500

    feat: add intended solution in comments

--- mergeIntervals/lc56.js
+++ mergeIntervals/lc56.js
@@ -103,3 +103,30 @@ for (i = 0; i < merged_intervals.length; i++) {
   result += merged_intervals[i].get_interval() + " ";
 }
 console.log(`Merged intervals: ${result}`);
+
+// Solution
+// -----
+// function merge(intervals) {
+//   if (intervals.length < 2) {
+//     return intervals;
+//   }
+//   // sort the intervals on the start time
+//   intervals.sort((a, b) => a.start - b.start);
+
+//   const mergedIntervals = [];
+//   let start = intervals[0].start,
+//     end = intervals[0].end;
+//   for (i = 1; i < intervals.length; i++) {
+//     const interval = intervals[i];
+//     if (interval.start <= end) { // overlapping intervals, adjust the 'end'
+//       end = Math.max(interval.end, end);
+//     } else { // non-overlapping interval, add the previous interval and reset
+//       mergedIntervals.push(new Interval(start, end));
+//       start = interval.start;
+//       end = interval.end;
+//     }
+//   }
+//   // add the last interval
+//   mergedIntervals.push(new Interval(start, end));
+//   return mergedIntervals;
+// }
