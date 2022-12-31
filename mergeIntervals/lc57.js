c2d61e708976d031b43cbbf23abc50cc25fb24e3
Jan Markus Milan <jmarkusm@me.com>
2022-12-30 23:50:46 -0500

    feat: provide class for problem

--- mergeIntervals/lc57.js
+++ mergeIntervals/lc57.js
@@ -1,3 +1,14 @@
 // Problem Statement #
 
 // Given a list of non-overlapping intervals sorted by their start time, insert a given interval at the correct position and merge all necessary intervals to produce a list that has only mutually exclusive intervals.
+
+class Interval {
+  constructor(start, end) {
+    this.start = start;
+    this.end = end;
+  }
+
+  print_interval() {
+    process.stdout.write(`[${this.start}, ${this.end}]`);
+  }
+}
