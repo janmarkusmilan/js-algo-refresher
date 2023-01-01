cf9cfd01628b754a8a056894197d17a108cc92f7
Jan Markus Milan <jmarkusm@me.com>
2022-12-31 23:47:18 -0500

    feat: add intended solution

--- mergeIntervals/lc57.js
+++ mergeIntervals/lc57.js
@@ -45,3 +45,34 @@ for (i = 0; i < result.length; i++) {
   result[i].print_interval();
 }
 console.log();
+
+// Solution
+// -----
+// function insert(intervals, new_interval) {
+//   let merged = [],
+//     i = 0;
+
+//   // skip and add to output) all intervals that come before the 'new_interval'
+//   while (i < intervals.length && intervals[i].end < new_interval.start) {
+//     merged.push(intervals[i]);
+//     i += 1;
+//   }
+
+//   // merge all intervals that overlap with 'new_interval'
+//   while (i < intervals.length && intervals[i].start <= new_interval.end) {
+//     new_interval.start = Math.min(intervals[i].start, new_interval.start);
+//     new_interval.end = Math.max(intervals[i].end, new_interval.end);
+//     i += 1;
+//   }
+
+//   // insert the new_interval
+//   merged.push(new_interval);
+
+//   // add all the remaining intervals to the output
+//   while (i < intervals.length) {
+//     merged.push(intervals[i]);
+//     i += 1;
+//   }
+
+//   return merged;
+// }
