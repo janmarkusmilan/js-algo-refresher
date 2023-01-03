ffad8e0e54d4d606b0a1e6a4c565e512df5a1be9
Jan Markus Milan <jmarkusm@me.com>
2023-01-02 23:55:49 -0500

    feat: provide test cases

--- cyclicSort/cyclicSortEasy.js
+++ cyclicSort/cyclicSortEasy.js
@@ -7,3 +7,8 @@
 const cyclic_sort = function (nums) {
   // TODO: Write your code here
 };
+
+console.log(`${cyclic_sort([3, 1, 5, 4, 2])}`);
+console.log(`${cyclic_sort([2, 6, 4, 3, 1, 5])}`);
+console.log(`${cyclic_sort([1, 5, 6, 4, 3, 2])}`);
+console.log(`${cyclic_sort([])}`);
