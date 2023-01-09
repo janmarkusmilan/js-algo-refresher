6182040cc48ef8d41cedb52996c65d8983b2030c
Jan Markus Milan <jmarkusm@me.com>
2023-01-08 23:59:38 -0500

    feat: add analysis of problem

--- cyclicSort/lc268.js
+++ cyclicSort/lc268.js
@@ -6,6 +6,34 @@
 /*
 Array: [0, 1, 3, 4]
 Missing Number: 2 <- return
+
+Observations:
+  - No duplicate values
+  - The range of numbers starts from 0 and is <= array length
+  - Asking for me to find a single number
+
+Edge Cases:
+  1. If empty array, return the array length
+  2. If every value matches it's index, return the array length
+
+Solving the problem:
+  1. Cyclic sort (every number is in correct index: index 0 -> 0, index 1 -> 1, ...)
+  2. The outlier will be in an incorrect index (index 2 -> 4, which is wrong)
+  3. After sorting, I can iterate through the array and just look for the number thats not in its correct index (arr[i] !== index)
+  4. Once I find the number, return the index
+
+Cyclic sort:
+  1. startIdx = 0;
+  2. Loop while startIdx < array length
+    3. Condition to check if the arr[i] is not equal to the index
+    4. swapIdx = arr[startIdx] - 1
+    5. Check for duplicates (not necessary since every number is distinct)
+    6. Swap using destructuring
+    7. Increment startIdx
+    
+[1, 0, 3, 4]
+Ideal: [0, 1, 2, 3]
+Cyclic Sort: [0, 1, 4, 3]
 */
 
 const find_missing_number = function (nums) {
