5b209a4665d97345dad29797640bdbb0d24959f4
Jan Markus Milan <jmarkusm@me.com>
2023-01-08 23:59:02 -0500

    feat: write sample example in comments

--- cyclicSort/lc268.js
+++ cyclicSort/lc268.js
@@ -3,6 +3,11 @@
 // We are given an array containing ‘n’ distinct numbers taken from the range 0 to ‘n’.
 // Since the array has only ‘n’ numbers out of the total ‘n+1’ numbers, find the missing number.
 
+/*
+Array: [0, 1, 3, 4]
+Missing Number: 2 <- return
+*/
+
 const find_missing_number = function (nums) {
   // TODO: Write your code here
 };
