1d92101b54a880ec06587f0af9873df4cd8c0bbc
Jan Markus Milan <jmarkusm@me.com>
2023-01-14 20:59:18 -0500

    feat: add analysis of question part 1

--- cyclicSort/lc287.js
+++ cyclicSort/lc287.js
@@ -3,6 +3,21 @@
 // We are given an unsorted array containing ‘n+1’ numbers taken from the range 1 to ‘n’. The array has only one duplicate but it can be repeated multiple times.
 // Find that duplicate number without using any extra space. You are, however, allowed to modify the input array.
 
+/*
+Question analysis:
+  1. Positive numbers in our array
+  2. Only one duplicate value (repeatable)
+  3. No extra space (don't create any extra scaling data structures)
+  4. The value of each number will be limited to the length of the array
+  5. You can modify the input array
+  6. Unsorted
+  7. Values are from 1 to n
+
+Edge cases:
+  1. If no duplicate: return -1
+  2. If empty array: return -1
+*/
+
 const find_duplicate = function (nums) {
   // TODO: Write your code here
 };
