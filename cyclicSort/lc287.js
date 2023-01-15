4b850789c9934a0e3ac10b2f45828c856482d769
Jan Markus Milan <jmarkusm@me.com>
2023-01-14 20:59:49 -0500

    feat: add analysis of question part 2

--- cyclicSort/lc287.js
+++ cyclicSort/lc287.js
@@ -16,6 +16,22 @@ Question analysis:
 Edge cases:
   1. If no duplicate: return -1
   2. If empty array: return -1
+
+Approaching the problem:
+  1. Sort the array using cyclic sort (each number will match it's index: index 0 -> 1, index 1 -> 2, ...)
+  2. When I do the cyclic sort, once I hit a duplicate number, I can just return that duplicate number
+  3. In traditional cyclic sort, once you hit a duplicate number, skip it <-- modify this one part to fit our question
+
+Cyclic Sort pseudo-code:
+  1. startIdx = 0
+  2. Loop until the startIdx reaches the end of our array
+      3. Have a condition that checks if the values don't match the index
+        4. If it doesn't match, then we initialize a swapIdx = array[startIdx] - 1
+        5. Condition to check for duplicates
+            6. If it is not a duplicate value, we swap using destructuring
+            7. If it is a duplicate value, return the duplicate value (originally increment startIdx by 1)
+        8. If it does match, increment startIdx by 1
+  9. Return -1 if we can't find our answer
 */
 
 const find_duplicate = function (nums) {
